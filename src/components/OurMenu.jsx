43d86c97058c58605e2e080eabdb5399cc21492d
ShimantoDevJS <shimantodevjs@gmail.com>
2023-12-17 20:08:01 +0600

    second commit

--- src/components/OurMenu.jsx
+++ src/components/OurMenu.jsx
@@ -1,39 +1,12 @@
-import { useEffect, useState } from 'react';
-import { fetchMenu } from '../api';
+import React from 'react'
 
 const OurMenu = () => {
-  const [burgerMenu, setBurgerMenu] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await fetchMenu();
-        console.log('API Response:', result);
-        const jsonData = JSON.parse(result);
-
-        // Check if the parsed data is an array before setting the state
-        if (Array.isArray(jsonData)) {
-          setBurgerMenu(jsonData);
-        } else {
-          console.error('Invalid menu data:', jsonData);
-        }
-      } catch (error) {
-        console.error('Error fetching menu:', error.message);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
       <h1 className='section-header'>Our Menu</h1>
-      <ul>
-        {Array.isArray(burgerMenu) &&
-          burgerMenu.map((burger, index) => <li key={index}>{burger.name}</li>)}
-      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default OurMenu;
+export default OurMenu
+
